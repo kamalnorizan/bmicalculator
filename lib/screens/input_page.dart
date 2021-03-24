2259aafab022c1848e0bc24937c92f37e651ba7c
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-03-24 11:37:23 +0800

    gender icons

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mybmicalculator/components/reusable_card.dart';
 
 class InputPage extends StatefulWidget {
@@ -22,11 +23,45 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1DE33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style:
+                              TextStyle(fontSize: 22.0, color: Colors.white70),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1DE33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'FEMALE',
+                          style:
+                              TextStyle(fontSize: 22.0, color: Colors.white70),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
